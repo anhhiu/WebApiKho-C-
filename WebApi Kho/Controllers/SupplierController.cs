fdb92a21db92b5057e90a7112fd4d6e69c3e9cee
anhhiu <dtc2054801360068@ictu.edu.vn>
2024-10-15 18:07:04 +0700

    supplier

--- WebApi Kho/Controllers/SupplierController.cs
+++ WebApi Kho/Controllers/SupplierController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 using WebApi_Kho.Databases;
 using WebApi_Kho.Models;
 
@@ -24,7 +25,6 @@ namespace WebApi_Kho.Controllers
         }
 
         [HttpPost]
-
         public async Task<ActionResult<Supplier>> CreateSupplier(Supplier supplier)
         {
             supplier.CreatedAt = DateTime.UtcNow;
@@ -36,6 +36,55 @@ namespace WebApi_Kho.Controllers
             return CreatedAtAction(nameof(GetSuppiler),new {id = supplier.Id}, supplier);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSuppilerById(int id)
+        {
+            var supplier = await context.Suppliers.FindAsync(id);
+
+            if (supplier == null) return NotFound();
+            
+            return Ok(supplier);
+
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<Supplier>> DeleteSupplier(int id)
+        {
+            var supplier =await context.Suppliers.FindAsync(id);
+
+            if (supplier == null) return NotFound();
+
+            context.Suppliers.Remove(supplier);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        
+
+        public async Task<ActionResult<Supplier>> UpdateSupplier(int id, Supplier supplier)
+        {
+            if (id != supplier.Id) return BadRequest();
+
+            context.Entry(supplier).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+              if  (!SupplierExits(id)) return BadRequest();
+              else throw;
+            }
+
+            return Ok(supplier);
+
+        }
+        private bool SupplierExits(int id) => context.Suppliers.Any(x => x.Id == id);
 
     }
 }
